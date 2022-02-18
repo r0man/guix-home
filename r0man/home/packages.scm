a5bd45b96d290023b536610192bb687cc7fca6bc
r0man <roman@burningswell.com>
2022-02-18 15:27:29 +0100

    Add docopt paimon and kubel

--- r0man/home/packages.scm
+++ r0man/home/packages.scm
@@ -4,15 +4,16 @@
   #:use-module (gnu packages certs)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages commencement)
+  #:use-module (gnu packages databases)
   #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages fonts)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages freedesktop)
-  #:use-module (gnu packages databases)
   #:use-module (gnu packages geo)
   #:use-module (gnu packages gimp)
   #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gnupg)
   #:use-module (gnu packages guile)
   #:use-module (gnu packages inkscape)
   #:use-module (gnu packages libffi)
@@ -22,10 +23,12 @@
   #:use-module (gnu packages mail)
   #:use-module (gnu packages man)
   #:use-module (gnu packages maths)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages terminals)
+  #:use-module (gnu packages tls)
   #:use-module (gnu packages uml)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages wm)
@@ -58,6 +61,7 @@
         emacs-docker
         emacs-docker-compose-mode
         emacs-dockerfile-mode
+        emacs-docopt
         emacs-edit-indirect
         emacs-el-mock
         emacs-elfeed
@@ -96,6 +100,7 @@
         emacs-js2-refactor
         emacs-json-process-client
         emacs-kotlin-mode
+        emacs-kubel
         emacs-language-detection
         emacs-lsp-dart
         emacs-lsp-docker
@@ -124,6 +129,7 @@
         emacs-org-tree-slide
         emacs-ox-jira
         emacs-ox-pandoc
+        emacs-paimon
         emacs-pandoc-mode
         emacs-paredit
         emacs-parsec
@@ -174,19 +180,25 @@
         glibc-locales
         gnu-make
         gnuplot
+        gnutls
         guile-3.0
         guile-git
+        guile-lzlib
+        guile-sqlite3
+        guile-zlib
         help2man
         inkscape
         isync
         lapack
         libatasmart
         libffi
+        libgcrypt
         libvterm
         mu
         nss-certs
         openblas
         openblas-ilp64
+        pkg-config
         plantuml
         python
         python-cython
