d1c534ef68aed96fc6e12ec8a085c850733fcc07
r0man <roman@burningswell.com>
2022-02-20 15:19:56 +0100

    Move packages to stumpwm

--- r0man/home/stumpwm.scm
+++ r0man/home/stumpwm.scm
@@ -1,7 +1,11 @@
 (define-module (r0man home stumpwm)
   #:use-module (gnu home services)
+  #:use-module (gnu packages lisp)
+  #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages wm)
   #:use-module (gnu services)
   #:use-module (guix gexp)
+  #:use-module (r0man packages lisp)
   #:export (home-stumpwm-services))
 
 (define files
@@ -10,5 +14,30 @@
     ("config/stumpwm/polybar.lisp" ,(local-file "files/stumpwm/polybar.lisp"))
     ("config/stumpwm/time.lisp" ,(local-file "files/stumpwm/time.lisp"))))
 
+(define packages
+  (list cl-stumpwm
+        sbcl
+        sbcl-local-time
+        sbcl-slime-swank
+        sbcl-stumpwm-battery-portable
+        sbcl-stumpwm-cpu
+        sbcl-stumpwm-disk
+        sbcl-stumpwm-globalwindows
+        sbcl-stumpwm-kbd-layouts
+        sbcl-stumpwm-mem
+        sbcl-stumpwm-net
+        sbcl-stumpwm-numpad-layouts
+        sbcl-stumpwm-pass
+        sbcl-stumpwm-screenshot
+        sbcl-stumpwm-stumptray
+        sbcl-stumpwm-swm-gaps
+        sbcl-stumpwm-ttf-fonts
+        sbcl-stumpwm-ttf-fonts
+        sbcl-stumpwm-wifi
+        sbcl-stumpwm-winner-mode
+        stumpish
+        stumpwm))
+
 (define home-stumpwm-services
-  (list (simple-service 'stumpwm home-files-service-type files)))
+  (list (simple-service 'stumpwm-config home-files-service-type files)
+        (simple-service 'stumpwm-packages home-profile-service-type packages)))
