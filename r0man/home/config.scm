0995a51742e4968bd7b806025710b44f82d6af6e
r0man <roman@burningswell.com>
2022-06-23 21:51:56 +0200

    Install channels

--- r0man/home/config.scm
+++ r0man/home/config.scm
@@ -2,6 +2,7 @@
   #:use-module (gnu home services)
   #:use-module (gnu home)
   #:use-module (gnu services)
+  #:use-module (guix gexp)
   #:use-module (r0man home bash)
   #:use-module (r0man home clojure)
   #:use-module (r0man home emacs)
@@ -14,9 +15,14 @@
   #:use-module (r0man home xdg)
   #:use-module (r0man home x11))
 
+(define home-channel-services
+  (list (simple-service 'channels-config home-files-service-type
+                        `((".config/guix/channels.scm" ,(local-file "channels.scm"))))))
+
 (define services
   (append
    home-bash-services
+   home-channel-services
    home-clojure-services
    home-emacs-services
    home-environment-variables-services
