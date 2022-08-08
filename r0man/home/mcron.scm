34ca4df762566eac598bd29162fbe4040f1bca8b
Roman Scherer <roman@burningswell.com>
2022-08-08 18:07:23 +0000

    Disable mcron gc, enable mbsync

--- r0man/home/mcron.scm
+++ r0man/home/mcron.scm
@@ -7,8 +7,12 @@
 (define guix-garbage-collect
   #~(job "0 12 * * *" "guix gc --delete-generations=1m"))
 
+(define mbsync
+  #~(job "* * * * *" "mbsync --all --verbose"))
+
+(define jobs
+  (list mbsync))
+
 (define home-mcron-services
-  (list (service
-         home-mcron-service-type
-         (home-mcron-configuration
-          (jobs (list guix-garbage-collect))))))
+  (list (service home-mcron-service-type
+                 (home-mcron-configuration (jobs jobs)))))
