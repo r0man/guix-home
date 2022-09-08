f1a3a98a082651e575550dc27b0bb31ebc13a106
Roman Scherer <roman@burningswell.com>
2022-09-08 10:59:42 +0000

    Configure GUILE_LOAD_COMPILED_PATH

--- r0man/home/environment.scm
+++ r0man/home/environment.scm
@@ -8,7 +8,8 @@
          'home-environment-variables-service
          home-environment-variables-service-type
          `(("EDITOR" . "emacsclient")
-           ("GUILE_LOAD_PATH" . "$HOME/.config/guix/current/share/guile/site/3.0:$GUILE_LOAD_PATH") ;; Fix: no code for module (guix i18n) in on-first-login :/
+           ("GUILE_LOAD_PATH" . "$HOME/.config/guix/current/share/guile/site/3.0:$GUILE_LOAD_PATH")
+           ("GUILE_LOAD_COMPILED_PATH" . "$HOME/.config/guix/current/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH")
            ("HISTCONTROL" . "ignoredups")
            ("HISTFILESIZE" . "10000000")
            ("HISTSIZE" . "100000")
