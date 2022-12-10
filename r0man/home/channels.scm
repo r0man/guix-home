acc86d6beab8f77faf4e64d983d9cdb28cea1c9d
r0man <roman@burningswell.com>
2022-12-10 12:58:04 +0100

    Use version-1.4.0 branch

--- r0man/home/channels.scm
+++ r0man/home/channels.scm
@@ -35,7 +35,8 @@
         (channel
          (name 'guix)
          (url "https://git.savannah.gnu.org/git/guix.git")
-         (branch "master")
+         ;; (branch "master")
+         (branch "version-1.4.0")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
