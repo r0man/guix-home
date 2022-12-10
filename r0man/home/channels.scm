965d407998b40094209667f072c9c2c3d5508966
r0man <roman@burningswell.com>
2022-12-10 13:46:14 +0100

    Go back to master branch

--- r0man/home/channels.scm
+++ r0man/home/channels.scm
@@ -35,8 +35,7 @@
         (channel
          (name 'guix)
          (url "https://git.savannah.gnu.org/git/guix.git")
-         ;; (branch "master")
-         (branch "version-1.4.0")
+         (branch "master")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
