83df1265e464589ba7e8391ae548c147795ecf7d
Roman Scherer <roman@burningswell.com>
2023-12-23 16:20:07 +0100

    Configure LS_COLORS

--- r0man/guix/home/bash.scm
+++ r0man/guix/home/bash.scm
@@ -1,6 +1,8 @@
 (define-module (r0man guix home bash)
   #:use-module (gnu home services shells)
+  #:use-module (gnu home services)
   #:use-module (gnu home)
+  #:use-module (gnu packages rust-apps)
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:export (home-bash-services))
@@ -20,4 +22,6 @@
           (bash-profile
            (list (local-file "files/bash_profile" "bash_profile")))
           (bash-logout
-           (list (local-file "files/bash_logout" "bash_logout")))))))
+           (list (local-file "files/bash_logout" "bash_logout")))))
+        (simple-service 'bash-packages home-profile-service-type
+                        (list vivid))))
