1b42fc8d9e796a1dec5ce763b51d1a6e06fd3980
Roman Scherer <roman@burningswell.com>
2024-02-25 19:04:49 +0100

    Configure xdg mime applications dynamically

--- r0man/guix/home/xdg.scm
+++ r0man/guix/home/xdg.scm
@@ -4,17 +4,25 @@
   #:use-module (guix gexp)
   #:use-module (guix modules))
 
+(define browser-mime-type
+  (let ((hostname (gethostname)))
+    (cond ((equal? "bombaclaat" hostname)
+           'icecat.desktop)
+          (else
+           'firefox.desktop))))
+
 ;; See: https://hg.sr.ht/~yoctocell/guixrc/browse/yoctocell/home/xdg.scm?rev=tip
 
 (define xdg-mime-applications-service
   (service home-xdg-mime-applications-service-type
            (home-xdg-mime-applications-configuration
-            (default '((application/pdf . org.gnome.Evince.desktop)
+            (default `((application/pdf . org.gnome.Evince.desktop)
                        (application/x-zoom . Zoom.desktop)
                        (inode/directory . emacsclient.desktop)
+                       (text/html . ,browser-mime-type)
                        (text/plain . emacsclient.desktop)
-                       (x-scheme-handler/http . firefox.desktop)
-                       (x-scheme-handler/https . firefox.desktop)
+                       (x-scheme-handler/http . ,browser-mime-type)
+                       (x-scheme-handler/https . ,browser-mime-type)
                        (x-scheme-handler/soundklaus emacsclient.desktop)
                        (x-scheme-handler/zoommtg . Zoom.desktop))))))
 
