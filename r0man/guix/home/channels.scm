3adebf7cb64efb5fb2980d4cbe1ca4e1f20e238e
Roman Scherer <roman@burningswell.com>
2024-05-23 10:00:26 +0200

    Use latest guix

--- r0man/guix/home/channels.scm
+++ r0man/guix/home/channels.scm
@@ -6,17 +6,27 @@
   #:export (channels home-channels-services))
 
 (define channels
-  (list (channel-with-substitutes-available
-         (channel
-          (name 'guix)
-          (url "https://git.savannah.gnu.org/git/guix.git")
-          (branch "master")
-          (introduction
-           (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-         "https://ci.guix.gnu.org")
+  (list ;; (channel-with-substitutes-available
+        ;;  (channel
+        ;;   (name 'guix)
+        ;;   (url "https://git.savannah.gnu.org/git/guix.git")
+        ;;   (branch "master")
+        ;;   (introduction
+        ;;    (make-channel-introduction
+        ;;     "9edb3f66fd807b096b48283debdcddccfea34bad"
+        ;;     (openpgp-fingerprint
+        ;;      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        ;;  "https://ci.guix.gnu.org")
+
+        (channel
+         (name 'guix)
+         (url "https://git.savannah.gnu.org/git/guix.git")
+         (branch "master")
+         (introduction
+          (make-channel-introduction
+           "9edb3f66fd807b096b48283debdcddccfea34bad"
+           (openpgp-fingerprint
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
 
         (channel
          (name 'nonguix)
