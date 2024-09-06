c65fbbec4a1264a48e35ce5e39fc042a5827798a
Roman Scherer <roman@burningswell.com>
2024-09-06 15:19:13 +0200

    Add imagemagick

--- r0man/guix/home/packages.scm
+++ r0man/guix/home/packages.scm
@@ -28,6 +28,7 @@
   #:use-module (gnu packages gnuzilla)
   #:use-module (gnu packages graphviz)
   #:use-module (gnu packages haskell-xyz)
+  #:use-module (gnu packages imagemagick)
   #:use-module (gnu packages inkscape)
   #:use-module (gnu packages java)
   #:use-module (gnu packages libffi)
@@ -106,6 +107,7 @@
    graphviz
    help2man
    htop
+   imagemagick
    inkscape
    ispell
    isync
