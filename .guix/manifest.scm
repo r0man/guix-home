6f967e3b6199b264a74dde2aab3ae2bb035d5f73
Roman Scherer <roman@burningswell.com>
2024-09-14 15:59:18 +0200

    Build only hello

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,5 +1,6 @@
-(use-modules (r0man guix system m1)
+(use-modules ;; (r0man guix system m1)
              (gnu system)
+             (gnu packages)
              (guix gexp)
              (guix git-download)
              (guix packages)
@@ -8,13 +9,15 @@
 
 (define %version "0.1.0")
 
-(define %m1-system
-  (manifest-entry
-    (name "m1-system")
-    (version %version)
-    (item m1-operating-system)))
+;; (define %m1-system
+;;   (manifest-entry
+;;     (name "m1-system")
+;;     (version %version)
+;;     (item m1-operating-system)))
 
-(define %systems
-  (manifest (list %m1-system)))
+;; (define %systems
+;;   (manifest (list %m1-system)))
 
-(concatenate-manifests (list %systems))
+;; (concatenate-manifests (list %systems))
+
+(specifications->manifest '("hello"))
