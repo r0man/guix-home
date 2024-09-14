fdf26126b62dd922620ef3ce922b71180e57f455
Roman Scherer <roman@burningswell.com>
2024-09-14 14:07:02 +0200

    Add .guix/manifest.scm

---
+++ .guix/manifest.scm
@@ -0,0 +1,20 @@
+(use-modules (r0man guix system m1)
+             (gnu system)
+             (guix gexp)
+             (guix git-download)
+             (guix packages)
+             (guix profiles)
+             (guix transformations))
+
+(define %version "0.1.0")
+
+(define %m1-system
+  (manifest-entry
+    (name "m1-system")
+    (version %version)
+    (item m1-operating-system)))
+
+(define %systems
+  (manifest (list %m1-system)))
+
+(concatenate-manifests (list %systems))
