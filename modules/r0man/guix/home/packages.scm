b8f372530eff92eeb647c18fcb278a4feac1e015
Roman Scherer <roman@burningswell.com>
2024-09-15 14:13:07 +0200

    Cleanup packages

--- modules/r0man/guix/home/packages.scm
+++ modules/r0man/guix/home/packages.scm
@@ -3,49 +3,33 @@
   #:use-module (gnu packages aspell)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
-  #:use-module (gnu packages bash)
   #:use-module (gnu packages certs)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages commencement)
   #:use-module (gnu packages compression)
-  #:use-module (gnu packages cpp)
   #:use-module (gnu packages databases)
   #:use-module (gnu packages dns)
   #:use-module (gnu packages docker)
-  #:use-module (gnu packages elixir)
   #:use-module (gnu packages file)
   #:use-module (gnu packages fonts)
-  #:use-module (gnu packages fonts)
-  #:use-module (gnu packages fontutils)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages geo)
-  #:use-module (gnu packages gettext)
   #:use-module (gnu packages gimp)
-  #:use-module (gnu packages glib)
-  #:use-module (gnu packages gnome)
   #:use-module (gnu packages gnupg)
-  #:use-module (gnu packages gnuzilla)
   #:use-module (gnu packages graphviz)
   #:use-module (gnu packages haskell-xyz)
   #:use-module (gnu packages imagemagick)
   #:use-module (gnu packages inkscape)
-  #:use-module (gnu packages java)
-  #:use-module (gnu packages libffi)
   #:use-module (gnu packages libreoffice)
   #:use-module (gnu packages linux)
-  #:use-module (gnu packages llvm)
   #:use-module (gnu packages mail)
   #:use-module (gnu packages man)
-  #:use-module (gnu packages maths)
-  #:use-module (gnu packages networking)
   #:use-module (gnu packages node)
   #:use-module (gnu packages package-management)
   #:use-module (gnu packages password-utils)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages pv)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages python-web)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages readline)
   #:use-module (gnu packages rust-apps)
@@ -56,11 +40,8 @@
   #:use-module (gnu packages texinfo)
   #:use-module (gnu packages tls)
   #:use-module (gnu packages tree-sitter)
-  #:use-module (gnu packages uml)
   #:use-module (gnu packages video)
-  #:use-module (gnu packages wm)
   #:use-module (gnu packages xdisorg)
-  #:use-module (gnu packages xorg)
   #:use-module (gnu packages)
   #:use-module (guix utils)
   #:use-module (r0man guix packages container)
@@ -80,9 +61,6 @@
    autoconf
    automake
    ;; awscli
-   bash
-   c2ffi
-   clang
    cmake
    coreutils
    docker-compose
@@ -110,9 +88,6 @@
    ispell
    isync
    libatasmart
-   libffi
-   libgcrypt
-   librdkafka
    libreoffice
    libvterm
    mu
@@ -123,7 +98,6 @@
    pgcli
    pkg-config
    plocate
-   po4a
    postgis
    postgresql
    pv
@@ -134,7 +108,6 @@
    ripgrep
    rlwrap
    rofi
-   rxvt-unicode
    sed
    sendgmail
    shared-mime-info
