5c8c1889cd822615b6d8f984be6690a658741847
Roman Scherer <roman@burningswell.com>
2024-10-21 13:56:23 +0200

    Use stumpwm-next

--- modules/r0man/guix/home/stumpwm.scm
+++ modules/r0man/guix/home/stumpwm.scm
@@ -5,8 +5,14 @@
   #:use-module (gnu packages wm)
   #:use-module (gnu services)
   #:use-module (guix gexp)
+  #:use-module (guix packages)
   #:use-module (r0man guix packages lisp)
+  #:use-module (r0man guix packages wm)
   #:export (home-stumpwm-services))
+
+(define-public replace-stumpwm
+  (package-input-rewriting/spec
+   `(("stumpwm" . ,(const stumpwm-next)))))
 
 (define files
   `((".config/stumpwm/commands.lisp" ,(local-file "files/stumpwm/commands.lisp"))
@@ -39,6 +45,33 @@
         sbcl-stumpwm-winner-mode
         stumpish
         stumpwm))
+
+(define packages
+  (append
+   (list sbcl
+         sbcl-local-time
+         sbcl-slime-swank
+         sbcl-slynk
+         stumpwm-next)
+   (map replace-stumpwm
+        (list sbcl-stumpwm-binwarp
+              sbcl-stumpwm-battery-portable
+              sbcl-stumpwm-cpu
+              sbcl-stumpwm-disk
+              sbcl-stumpwm-globalwindows
+              sbcl-stumpwm-kbd-layouts
+              sbcl-stumpwm-mem
+              sbcl-stumpwm-net
+              sbcl-stumpwm-numpad-layouts
+              sbcl-stumpwm-pamixer
+              sbcl-stumpwm-pass
+              sbcl-stumpwm-screenshot
+              sbcl-stumpwm-stumptray
+              sbcl-stumpwm-swm-gaps
+              sbcl-stumpwm-ttf-fonts
+              sbcl-stumpwm-wifi
+              sbcl-stumpwm-winner-mode
+              stumpish))))
 
 (define home-stumpwm-services
   (list (simple-service 'stumpwm-config home-files-service-type files)
