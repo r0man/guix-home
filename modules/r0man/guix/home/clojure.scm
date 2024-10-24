59cb5ecdb5b7e038afc92116d6fcb981afb8e1b0
Roman Scherer <roman@burningswell.com>
2024-10-24 18:52:21 +0200

    Remove clojure module

--- modules/r0man/guix/home/clojure.scm
+++ modules/r0man/guix/home/clojure.scm
@@ -4,7 +4,6 @@
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:use-module (nongnu packages clojure)
-  #:use-module (r0man guix packages clojure)
   #:export (home-clojure-services))
 
 ;; Clojure
