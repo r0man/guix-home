12ea485123f8a212f810a9b82eb7708c28ab06f7
Roman Scherer <roman@burningswell.com>
2024-12-21 13:56:34 +0100

    Use emacs-next-pgtk

--- modules/r0man/guix/home/emacs.scm
+++ modules/r0man/guix/home/emacs.scm
@@ -13,7 +13,7 @@
   #:export (emacs-shepherd-service home-emacs-services))
 
 (define packages
-  (list emacs-pgtk
+  (list emacs-next-pgtk
         emacs-aider
         emacs-adoc-mode
         emacs-aggressive-indent
