50b95440bbe25acbce9dd132596bd50e7a375eed
Roman Scherer <roman@burningswell.com>
2025-06-22 10:55:41 +0200

    Add desktop entries for claude and zstray

--- modules/r0man/guix/home/xdg.scm
+++ modules/r0man/guix/home/xdg.scm
@@ -16,11 +16,30 @@
                        (inode/directory . emacsclient.desktop)
                        (text/html . ,browser-mime-type)
                        (text/plain . emacsclient.desktop)
+                       (x-scheme-handler/claude . ClaudeDesktop.desktop)
                        (x-scheme-handler/http . ,browser-mime-type)
                        (x-scheme-handler/https . ,browser-mime-type)
                        (x-scheme-handler/slack . com.slack.Slack.desktop)
                        (x-scheme-handler/soundklaus . emacsclient.desktop)
-                       (x-scheme-handler/zoommtg . us.zoom.Zoom.desktop))))))
+                       (x-scheme-handler/zoommtg . us.zoom.Zoom.desktop)
+                       (x-scheme-handler/zsa . ZSTray.desktop)))
+            (desktop-entries
+             (list (xdg-desktop-entry
+                    (file "ClaudeDesktop")
+                    (name "Claude Desktop")
+                    (type 'application)
+                    (config
+                     '((exec . "/usr/bin/claude-desktop %u")
+                       (MimeType . "x-scheme-handler/claude;")
+                       (Terminal . "false"))))
+                   (xdg-desktop-entry
+                    (file "ZSTray")
+                    (name "ZScaler Client Connector")
+                    (type 'application)
+                    (config
+                     '((exec . "/opt/zscaler/scripts/zstray_desktop.sh %U")
+                       (MimeType . "x-scheme-handler/zsa;application/x-zstray;")
+                       (Terminal . "false")))))))))
 
 (define-public home-xdg-services
   (list xdg-mime-applications-service))
