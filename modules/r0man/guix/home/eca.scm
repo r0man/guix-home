1cab213610baf018149ad817df96bf1abb113e66
Roman Scherer <roman@burningswell.com>
2025-10-09 18:27:18 +0200

    Manage eca config

--- modules/r0man/guix/home/eca.scm
+++ modules/r0man/guix/home/eca.scm
@@ -6,7 +6,8 @@
 
 (define files
   `((".config/eca/AGENTS.md" ,(local-file "files/eca/AGENTS.md"))
-    (".config/eca/commands" ,(local-file "files/eca/commands" #:recursive? #t))))
+    (".config/eca/commands" ,(local-file "files/eca/commands" #:recursive? #t))
+    (".config/eca/config.json" ,(local-file "files/eca/config.json"))))
 
 (define home-eca-services
   (list (simple-service 'eca-config home-files-service-type files)))
