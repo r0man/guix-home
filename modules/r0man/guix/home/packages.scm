6eb69d15011ca87f91dac27950f0531bd6954b40
Roman Scherer <roman@burningswell.com>
2025-10-10 17:44:37 +0200

    Add node-anthropic-ai-claude-code

--- modules/r0man/guix/home/packages.scm
+++ modules/r0man/guix/home/packages.scm
@@ -57,6 +57,7 @@
   #:use-module (gnu packages)
   #:use-module (guix utils)
   #:use-module (r0man guix packages container)
+  #:use-module (r0man guix packages node)
   #:export (packages-aarch64
             packages-base
             packages-desktop
@@ -96,6 +97,7 @@
         mu
         mumi
         ncurses
+        node-anthropic-ai-claude-code
         node-lts
         nss-certs
         openssl
