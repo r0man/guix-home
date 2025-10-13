00a6748bb6af210ad3856495932f0f812b9c62bb
Roman Scherer <roman@burningswell.com>
2025-10-13 09:54:16 +0200

    Add home whisper server to precision

--- modules/r0man/guix/channels.scm
+++ modules/r0man/guix/channels.scm
@@ -15,12 +15,14 @@
 (define-public guix-channel
   (channel
    (name 'guix)
-   (url "https://git.guix.gnu.org/guix.git")
-   (introduction
-    (make-channel-introduction
-     "9edb3f66fd807b096b48283debdcddccfea34bad"
-     (openpgp-fingerprint
-      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+   ;; (url "https://git.guix.gnu.org/guix.git")
+   ;; (introduction
+   ;;  (make-channel-introduction
+   ;;   "9edb3f66fd807b096b48283debdcddccfea34bad"
+   ;;   (openpgp-fingerprint
+   ;;    "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))
+   (url "https://codeberg.org/r0man/guix")
+   (branch "add-whisper-service")))
 
 (define-public nonguix-channel
   (channel
