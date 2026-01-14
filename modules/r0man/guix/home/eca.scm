7b626b572c3079e431a124bc751d5c05cec14d8e
Roman Scherer <roman@burningswell.com>
2026-01-14 20:13:41 +0100

    Enhance home-eca service with package installation
    - Add editor-code-assistant package to home-eca service profile
    - Rename eca-with-authinfo to eca-server-authinfo
    - Pass command-line arguments through to eca server
    - Update Emacs configuration to use new script name

--- modules/r0man/guix/home/eca.scm
+++ modules/r0man/guix/home/eca.scm
@@ -3,6 +3,7 @@
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:use-module (guix records)
+  #:use-module (r0man guix packages clojure)
   #:export (home-eca-configuration
             home-eca-service-type))
 
@@ -16,6 +17,9 @@
 (define-record-type* <home-eca-configuration>
   home-eca-configuration make-home-eca-configuration
   home-eca-configuration?
+  (package home-eca-package
+           (default editor-code-assistant)
+           (description "Package to install for eca."))
   (agents-file home-eca-agents-file
                (default (local-file "files/eca/AGENTS.md"))
                (description "Path to AGENTS.md file."))
@@ -24,20 +28,30 @@
                 (description "Path to commands directory."))
   (config-file home-eca-config-file
                (default (local-file "files/eca/config.json"))
-               (description "Path to config.json file.")))
+               (description "Path to config.json file."))
+  (authinfo-script home-eca-authinfo-script
+                   (default (local-file "files/eca/eca-server-authinfo"))
+                   (description "Path to eca-server-authinfo script.")))
 
 (define (home-eca-files config)
   "Return alist of eca configuration files to deploy."
   `((".config/eca/AGENTS.md" ,(home-eca-agents-file config))
     (".config/eca/commands" ,(home-eca-commands-dir config))
-    (".config/eca/config.json" ,(home-eca-config-file config))))
+    (".config/eca/config.json" ,(home-eca-config-file config))
+    (".local/bin/eca-server-authinfo" ,(home-eca-authinfo-script config))))
+
+(define (home-eca-profile config)
+  "Return list of packages to install for eca."
+  (list (home-eca-package config)))
 
 (define home-eca-service-type
   (service-type
    (name 'home-eca)
    (extensions
     (list (service-extension home-files-service-type
-                             home-eca-files)))
+                             home-eca-files)
+          (service-extension home-profile-service-type
+                             home-eca-profile)))
    (default-value (home-eca-configuration))
    (description
     "Install and configure Editor Code Assistant (eca) for the user.")))
