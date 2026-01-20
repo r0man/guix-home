4976b8b64c2c49ce7733eb8d26282076a563d419
Roman Scherer <roman@burningswell.com>
2026-01-20 13:06:10 +0100

    Add guix-channel-r0man

--- modules/r0man/guix/channels.scm
+++ modules/r0man/guix/channels.scm
@@ -23,6 +23,17 @@
      (openpgp-fingerprint
       "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
 
+(define-public guix-channel-r0man
+  (channel
+   (name 'guix)
+   (url "https://codeberg.org/r0man/guix")
+   (branch "add-whisper-service")
+   (introduction
+    (make-channel-introduction
+     "9edb3f66fd807b096b48283debdcddccfea34bad"
+     (openpgp-fingerprint
+      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+
 (define-public nonguix-channel
   (channel
    (name 'nonguix)
