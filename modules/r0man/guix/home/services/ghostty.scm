a13e2594244de451336b41471eac629753c40818
Roman Scherer <roman@burningswell.com>
2026-05-07 13:06:22 +0200

    wip

--- modules/r0man/guix/home/services/ghostty.scm
+++ modules/r0man/guix/home/services/ghostty.scm
@@ -1,6 +1,7 @@
 (define-module (r0man guix home services ghostty)
   #:use-module (gnu home services)
   #:use-module (gnu packages bash)
+  #:use-module (gnu services configuration)
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:use-module (guix packages)
@@ -16,37 +17,37 @@
 ;;;
 ;;; Code:
 
-(define-record-type* <home-ghostty-configuration>
-  home-ghostty-configuration make-home-ghostty-configuration
-  home-ghostty-configuration?
-  (background-opacity home-ghostty-background-opacity
-                      (default 0.7)
-                      (description "Background opacity (0.0 to 1.0)."))
-  (font-family home-ghostty-font-family
-               (default "Hack")
-               (description "Font family name."))
-  (font-size home-ghostty-font-size
-             (default 14.0)
-             (description "Font size in points."))
-  (foreground home-ghostty-foreground
-              (default "#586e75")
-              (description "Foreground text color."))
-  (command home-ghostty-command
-           (default (file-append bash "/bin/bash --login"))
-           (description "Initial command (shell) to run in ghostty."))
-  (packages home-ghostty-packages
-            (default (list ghostty))
-            (description "List of ghostty-related packages to install.")))
+(define-configuration/no-serialization home-ghostty-configuration
+  (background-opacity
+   (number 0.7)
+   "Background opacity (0.0 to 1.0).")
+  (font-family
+   (string "Hack")
+   "Font family name.")
+  (font-size
+   (number 14.0)
+   "Font size in points.")
+  (foreground
+   (string "#586e75")
+   "Foreground text color.")
+  (command
+   (file-like (file-append bash "/bin/bash --login"))
+   "Initial command (shell) to run in ghostty.")
+  (packages
+   (list-of-packages (list ghostty))
+   "List of ghostty-related packages to install."))
 
 (define (home-ghostty-config config)
   "Generate ghostty config file from CONFIG."
-  (mixed-text-file
-   "ghostty-config"
-   "background-opacity = " (number->string (home-ghostty-background-opacity config)) "\n"
-   "font-family = " (home-ghostty-font-family config) "\n"
-   "font-size = " (number->string (home-ghostty-font-size config)) "\n"
-   "foreground = " (home-ghostty-foreground config) "\n"
-   "command = " (home-ghostty-command config) "\n"))
+  (match-record config <home-ghostty-configuration>
+                (background-opacity font-family font-size foreground command)
+    (mixed-text-file
+     "ghostty-config"
+     "background-opacity = " (number->string background-opacity) "\n"
+     "font-family = " font-family "\n"
+     "font-size = " (number->string font-size) "\n"
+     "foreground = " foreground "\n"
+     "command = " command "\n")))
 
 (define (home-ghostty-files config)
   "Return alist of ghostty configuration files to deploy."
@@ -54,7 +55,7 @@
 
 (define (home-ghostty-profile-packages config)
   "Return list of ghostty packages to install."
-  (home-ghostty-packages config))
+  (home-ghostty-configuration-packages config))
 
 (define home-ghostty-service-type
   (service-type
